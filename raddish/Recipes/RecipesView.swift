cb097a7cfc90e2787b3578369907bfd2f611bd0e
Esteban Vazquez Cardona <estebanv@ww109-a10.andrew.cmu.edu>
2021-11-15 18:23:20 -0500

    fixed async image in recipes detail view

--- raddish/Recipes/RecipesView.swift
+++ raddish/Recipes/RecipesView.swift
@@ -29,30 +29,7 @@ struct RecipesView: View {
                                                placeholder: { Text("Loading...") })
                                       .frame(width: 107, height: 115)
                                       .cornerRadius(15)
-//                                    AsyncImage(
-//                                      url: URL(string: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg")!,
-//                                                    placeholder: {Text("Loading ...")}
-//                                                ).aspectRatio(contentMode: .fit)
-//                                        URLImage(url: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg")
-//                                          .frame(width: 107, height: 115)
-//                                          .cornerRadius(15)
-//                                      if #available(iOS 15.0, *) {
-//                                        AsyncImage(url: URL(string: i.strMealThumb ))
-//                                        { image in
-//                                            image.resizable()
-//                                        } placeholder: {
-//                                            ProgressView()
-//                                        }
-//                                        .frame(width: 107, height: 115)
-//                                        .cornerRadius(15)
-//                                        TODO: shadow
-//                                        .shadow(color: Color.black.opacity(0.3), radius: 20, x: -10, y: 10)
-//                                    
-//                                  }
-//                                       else {
-//                                        fatalError("Oh no! Upgrade your phone")
-//                                      }
-                                    }
+                                  }
                                   .sheet(isPresented: $showingSheet) {
                                     RecipeDetailsView(recipe: i)
                                 }
