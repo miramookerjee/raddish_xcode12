88975097d9a3c5848ea926b6984e9e50a0389a88
Ankita Kundu <ankitaku@andrew.cmu.edu>
2021-11-15 18:39:57 -0500

    Added in a datepicker thing

--- raddish/Pantry/AddPantryItem.swift
+++ raddish/Pantry/AddPantryItem.swift
@@ -11,13 +11,17 @@ import SwiftUI
 import UIKit
 
 struct AddPantryItem: View {
-
+    let dateFormatter: DateFormatter = {
+           let formatter = DateFormatter()
+           formatter.dateStyle = .long
+           return formatter
+       }()
   var viewModel: ViewModel
   @Environment(\.presentationMode) var mode: Binding<PresentationMode>
 
   @State var name: String = ""
   @State var expiration: NSDate?
-  @State var date: Date?
+  @State private var date = Date()
 
   var body: some View {
     VStack {
@@ -27,22 +31,19 @@ struct AddPantryItem: View {
           .padding(.leading)
         TextField("name", text: $name)
           .padding(.trailing)
-      
-        Text("date:")
-          .fontWeight(.bold)
-          .padding(.leading)
-        //DatePicker(
-            //"Start Date",
-            //selection: $expiration ?? Date() as Date?,
-            //displayedComponents: [.date]
-           //)
-      }.padding()
-      Spacer()
+    
+        DatePicker(
+            "Start Date",
+            selection: $date,
+            displayedComponents: .date
+           )
+      }
+      //Spacer()
     }.padding()
     .navigationBarTitle("New Pantry Item")
     .navigationBarItems(trailing:
       Button(action: {
-      self.viewModel.savePantryItem(name: self.name, expiration: Date() as NSDate, date: Date())
+        self.viewModel.savePantryItem(name: self.name, expiration: NSDate(), date: date)
       self.mode.wrappedValue.dismiss()
       }) {
         Text("Done")
