63906252cbe619ff1031cda52f90666c7d8252c0
Esteban Vazquez Cardona <estebanv@ww109-a1.andrew.cmu.edu>
2021-11-17 13:42:26 -0500

    need to fix issue with UUID to remove correct item

--- raddish/Resources/ViewModel.swift
+++ raddish/Resources/ViewModel.swift
@@ -237,10 +237,13 @@ class ViewModel: ObservableObject {
       do {
         let result = try context.fetch(request)
         for data in result as! [NSManagedObject] {
-          print(data)
+          // TODO: fix removing UUID issue
+//          print(data.value(forKey: "id"))
+//          print(mealItem.id)
           if ((mealItem.imageURL == data.value(forKey: "imageURL") as? String)
               && (mealItem.name == data.value(forKey: "name") as? String)
               && (mealItem.day_of_week == data.value(forKey: "day_of_week") as? String)
+//              && (mealItem.id == data.value(forKey: "id") as? UUID)
           )
            {
             context.delete(data)
