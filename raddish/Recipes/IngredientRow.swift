644ec7303503fdc7a62029c44b9f9a0c6fbfcfae
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-12-02 16:16:53 -0500

    added ingredient images

--- raddish/Recipes/IngredientRow.swift
+++ raddish/Recipes/IngredientRow.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct IngredientRow: View {
+  @ObservedObject var viewModel: ViewModel
   var ingredient : String
   var measure: String
     var body: some View {
       if (measure != "" && ingredient != "") {
         HStack {
+          AsyncImage(url: URL(string: viewModel.ingredientImages(ingredient:ingredient))!,
+                     placeholder: { Text("Loading...") })
+              .frame(width: 40, height: 40)
+              .cornerRadius(15)
+            .padding()
           Text(measure)
             .padding()
           Text(ingredient)
